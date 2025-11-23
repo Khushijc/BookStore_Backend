dd89b8637782c619ad461b46d162ad028106115c
Khushijc <khushijc02@gmail.com>
2025-11-23 18:47:21 +0530

    Your message (e.g. Added CORS support)

--- api/index.js
+++ api/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from 'express';
 import cors from 'cors';
 import connectDB from '../config/db.js';
@@ -40,31 +39,3 @@ app.use('/api/ai', aiRoutes);
 app.listen(PORT, () => {
     console.log(`Server is running at ${PORT}`);
 });
-=======
-import express from 'express';
-import connectDB from '../config/db.js';
-import authRoutes from "../routes/auth.routes.js"
-import booksRoutes from "../routes/book.routes.js"
-import orderRoutes from "../routes/order.routes.js"
-import aiRoutes from "../routes/ai.routes.js"
-
-const app=express();
-
-app.use(express.json());
-connectDB()
-
-const PORT=3000;
-
-app.get("/",(req,res)=>{
-    res.send("Bookstore Api is running");
-});
-
-app.use('/api/auth',authRoutes)
-app.use("/api/books", booksRoutes)
-app.use("/api/order", orderRoutes)
-app.use("/api/ai", aiRoutes)
-
-app.listen(PORT,()=>{
-   console.log(`Server is running at ${PORT}`) ;
-});
->>>>>>> 266eacda04f6dcbe394253cfe4fb903f9a389e9c
